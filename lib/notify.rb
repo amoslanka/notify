a9fe205e29e10a43f267dcf0e413d5933a6948fc
Amos Lanka <amoslanka@gmail.com>
2014-11-13 21:44:54 -0800

    initial commit

---
+++ lib/notify.rb
@@ -0,0 +1,14 @@
+module Notify
+  class Engine < ::Rails::Engine
+
+
+    # Configure Generators
+    config.generators do |g|
+      g.test_framework :rspec, fixture: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.assets false
+      g.helper false
+    end
+
+  end
+end
